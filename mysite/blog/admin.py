13985ab7bcaf0e2399361fc55f7389e46a306116
mazhaoyan <mazhaoyansky@163.com>
2017-06-25 22:24:43 +0800

    mysite

---
+++ mysite/blog/admin.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from blog import models
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'timestamp')
+admin.site.register(models.BlogPost, BlogPostAdmin)
+
