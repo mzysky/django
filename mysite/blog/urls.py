7464322d9f4b6256416bc51ef02c3c5c1717a6be
Zhaoyan Ma/马兆岩/PD/SMART HOME <zhaoyan.ma@freecomm-networks.com>
2017-06-26 20:28:14 +0800

    title 11.11.4

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls  import url, include
 import blog.views
 
 urlpatterns = [
+        url(r'^create/', blog.views.create_blogpost),
         url(r'^$', blog.views.archive),
 ]
 
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,6 +5,17 @@ from django.shortcuts import render, render_to_response
 from datetime import datetime
 from blog.models import BlogPost
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 def archive(request):
-    posts = BlogPost.objects.all().order_by("title") #order the data
+    posts = BlogPost.objects.all().order_by("-title") #order the data
     return render_to_response("archive.html", {'posts':posts})
+def create_blogpost(request):
+    print "***************"
+    if request.method == 'POST':
+        BlogPost(
+                title = request.POST.get('title'),
+                body = request.POST.get('body'),
+                timestamp = datetime.now(),
+                ).save()
+    return HttpResponseRedirect('/blog/')
+
